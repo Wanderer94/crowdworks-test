2443ed7170195a3daf4e982e025be881b8bf3f7b
JunwooPark <wnsdn1994@gmail.com>
2024-10-31 16:48:34 +0900

    chore(crowdworks): 초기세팅 완료

---
+++ crowdworks-image-api/src/test/java/com/example/crowdworksimageapi/CrowdworksImageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crowdworksimageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdworksImageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
